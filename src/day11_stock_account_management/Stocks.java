536a12e49580fb8a2f8e763dedc5ab5e5471d2bb
Manju31051996 <bbelagavi6@gmail.com>
2022-12-13 22:00:57 +0530

    [Manjunath S B] Day 11 and 12 Programs ->Created the Stock List to access the Stock List - Stock Account Management Syatem

---
+++ src/day11_stock_account_management/Stocks.java
@@ -0,0 +1,15 @@
+package day11_stock_account_management;
+
+import java.util.List;
+
+public class Stocks {
+    private List<Stock> stockList;
+
+    public List<Stock> getStockList() {
+        return stockList;
+    }
+
+    public void setStockList(List<Stock> stockList) {
+        this.stockList = stockList;
+    }
+}

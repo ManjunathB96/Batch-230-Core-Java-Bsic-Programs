e2a25a6a313a099e2c7a0a51965410573478809e
Manju31051996 <bbelagavi6@gmail.com>
2022-12-14 10:49:34 +0530

    [Manjunath S B]Day3->commited the reason why RemoveDupChar program is printing ASCII code along with respective Decimal Unique code

--- src/day3_array_based_programs/RemoveDupChar.java
+++ src/day3_array_based_programs/RemoveDupChar.java
@@ -28,7 +28,15 @@ public class RemoveDupChar {
       /*  for (int array : newArr){
             System.out.print(array+" ");
             }
-     //output is 109 97 110 106 117 0 0 0 0 the numbers it is printing bcz it taking ascii value i,e unique code
+     //output is 109 97 110 106 117 0 0 0 0 the numbers it is printing bcz it taking ASCII value i,e unique code
+     these values only for lower case letters and not  for UpperCase letters  The code values are different for UpperCase letters
+    char -> m -> decimal -> 109  or hex->6D  or oct->155
+     char -> a -> decimal -> 97  or hex->61  or oct->141
+      char -> n -> decimal -> 110  or hex->6E  or oct->156
+       char -> j -> decimal -> 106  or hex->6A  or oct->152
+        char -> u -> decimal -> 117  or hex->75  or oct->165
+
+
 */
         for (int array : newArr) {
             System.out.print((char) array + " "); //output : m a n j u

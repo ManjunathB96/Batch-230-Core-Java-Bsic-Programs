149abedb5a17f97c21e37af2fbe2290070d8947b
Manju31051996 <bbelagavi6@gmail.com>
2022-12-23 00:24:01 +0530

    [Manjunath S B] Day-19 ->Regex: List Of Email Validation Check UC files added

---
+++ src/day_19_regex_email_validation_programs/EmailValidation17.java
@@ -0,0 +1,20 @@
+package day_19_regex_email_validation_programs;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EmailValidation17 {
+    public static void main(String[] args) {
+        String emailId = "abc@%*.com";
+
+        Pattern pattern = Pattern.compile("^([a-zA-Z]{3,}([.|_|+|-]?[a-zA-Z0-9]+)?[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.]?[a-zA-Z]{2,3})?)$");
+        Matcher matcher = pattern.matcher(emailId);
+
+        if (matcher.matches() == true) {
+            System.out.println(emailId + "  " + " Result->" + matcher.matches() + " " + "Valid Email Id");
+        } else {
+            System.out.println(emailId + "  " + "Result->" + matcher.matches());
+            System.out.println("Please Enter Valid Email Id....!");
+        }
+    }
+}
